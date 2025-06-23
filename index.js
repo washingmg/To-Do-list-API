2c2844a8f3b19513c33ad945c1cdde50109bbd7c
Washington Medeiros <wmedeirosm001@gmail.com>
2025-06-23 17:44:43 -0300

    feat: app config

---
+++ index.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+import express, { json, urlencoded } from "express";
+import "express-async-errors";
+import cors from "cors";
+
+dotenv.config();
+
+// import database from "./app/database/index.js";
+// import routes from "./app/routes/index.js"; // Import API routes
+
+// (async () => {
+//     await database();
+// })();
+
+const app = express();
+
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+};
+
+app.use(cors(corsOptions));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+// Inject API Routes
+// app.use("/api", routes);
+
+app.get("/", (request, response) => {
+    response.json({ message: "Welcome to ToDo-Simple NodeJS API." });
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use(function (error, request, response, next) {
+    if (process.env.APP_DEBUG) {
+        console.error(error);
+        return response.status(error.statusCode ?? 500).json({
+            status: "Error",
+            message: error.message,
+            error: error,
+        });
+    } else {
+        return response.status(error.statusCode ?? 500).json({
+            status: "Error",
+            message: error.message,
+        });
+    }
+});
+
+const port = process.env.NODE_LOCAL_PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`ToDoSimple NodeJS API listening on port ${port}`);
+    console.log(`---> http://${process.env.NODE_APP_HOST}:${port}`);
+});
